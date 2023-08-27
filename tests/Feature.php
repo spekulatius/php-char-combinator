d5f70fc37ca770ce57c1b33663c344070d368bfa
Peter <peter.thaleikis@gmail.com>
2023-08-27 18:00:14 +0200

    wip

--- tests/Feature.php
+++ tests/Feature.php
@@ -1,11 +1,31 @@
 <?php
 
-use NunoMaduro\SkeletonPhp\Example;
+use Spekulatius\PHPCharCombinator\Combinator;
 
-it('foo', function () {
-    $example = new Example();
+it('generates combinations of lengths 1 to 3', function () {
+    $combinator = new Combinator();
+    $combinations = $combinator->generateCombinations(1, 3);
 
-    $result = $example->foo();
+    expect($combinations)->toBeArray();
+    expect(count($combinations))->toBeGreaterThan(0);
+    expect(in_array('a', $combinations))->toBeTrue(); // Check for specific combinations
+    expect(in_array('abc', $combinations))->toBeTrue();
+});
 
-    expect($result)->toBe('bar');
+it('generates combinations of lengths 4 and 5', function () {
+    $combinator = new Combinator();
+    $combinations = $combinator->generateCombinations(4, 5);
+
+    expect($combinations)->toBeArray();
+    expect(count($combinations))->toBeGreaterThan(0);
+    expect(in_array('abcd', $combinations))->toBeTrue(); // Check for specific combinations
+    expect(in_array('abcde', $combinations))->toBeTrue();
+});
+
+it('generates empty combinations when startLevels is greater than endLevels', function () {
+    $combinator = new Combinator();
+    $combinations = $combinator->generateCombinations(3, 2);
+
+    expect($combinations)->toBeArray();
+    expect($combinations)->toBeEmpty();
 });
