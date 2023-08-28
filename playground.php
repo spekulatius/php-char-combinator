e33ec278d7192884f0d3d3eec822ee672306217e
Peter <peter.thaleikis@gmail.com>
2023-08-28 08:39:35 +0200

    Expanding playground

--- playground.php
+++ playground.php
@@ -17,6 +17,27 @@ $combinations = $combinator->generateCombinations(
     3,
 );
 
-foreach ($combinations as $combination) {
+// Prep
+$channel = $argv[1];
+$result = [];
 
+echo count($combinations) . " combinations to test...\n";
+
+foreach ($combinations as $index => $combination) {
+    // Create the complete string to work with.
+    $finalString = str_repeat($combination, 10000);
+
+    // Track time usage.
+    $start = microtime(true);
+
+
+    // End tracking
+    $end = microtime(true);
+    $duration = round(($end - $start)*1000000, 2);
+
+    if ($index / 1000 === (int) ($index / 1000)) echo "Test #{$index}: {$duration} ms\n";
+
+    if ($duration > 100000) $result[$combination] = $duration;
 }
+
+file_put_contents('./' . $channel . '.json', json_encode($result, JSON_PRETTY_PRINT));
